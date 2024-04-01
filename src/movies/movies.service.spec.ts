b554225086119091ffa51a7cea201816c2cea698
Dasha <dasha@peoplcat.com>
2024-04-01 18:08:45 +0900

    test: nomadcoders lecture 3.4

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
+import { NotFoundException } from '@nestjs/common';
 
 describe('MoviesService', () => {
   let service: MoviesService;
@@ -14,5 +15,89 @@ describe('MoviesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAll', () => {
+    it('should return an array', () => {
+      const result = service.getAll();
+
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne', () => {
+    it('should return a movie', () => {
+      service.createMovie({
+        title: 'Test',
+        genres: ['test'],
+        year: 2024,
+      }); // getOne을 위한 객체 생성
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    });
+    it('should throw 404 error', () => {
+      try {
+        service.getOne(999);
+      } catch (err) {
+        expect(err).toBeInstanceOf(NotFoundException);
+        expect(err.message).toEqual('해당하는 영화를 찾을 수 없습니다.');
+      }
+    });
+  });
+
+  describe('deleteOne', () => {
+    it('deletes a movie', () => {
+      service.createMovie({
+        title: 'Test',
+        genres: ['test'],
+        year: 2024,
+      }); // 지우기 위한 객체 생성
+      const beforeDelete = service.getAll();
+      service.deleteMovie(1);
+      const afterDelete = service.getAll();
+
+      expect(afterDelete.length).toEqual(beforeDelete.length - 1);
+    });
+
+    it('should return a 404', () => {
+      try {
+        service.deleteMovie(999);
+      } catch (err) {
+        expect(err).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  describe('create', () => {
+    it('should create a movie', () => {
+      const beforeCreate = service.getAll().length;
+      service.createMovie({
+        title: 'Test',
+        genres: ['test'],
+        year: 2024,
+      });
+
+      const afterCreate = service.getAll().length;
+
+      expect(afterCreate).toEqual(beforeCreate + 1);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a movie', () => {
+      service.createMovie({
+        title: 'Test',
+        genres: ['test'],
+        year: 2024,
+      });
+
+      service.updateMovie(1, {
+        title: 'Update Test',
+      });
+
+      const movie = service.getOne(1);
+      expect(movie.title).toEqual('Update Test');
+    });
   });
 });
